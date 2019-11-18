d8669b5c063ac57b83492f3ba5d1301a04698d15
Tom Hill <tomhill98@me.com>
2019-11-18 22:18:54 +1100

    make it better

--- models.py
+++ models.py
@@ -1,8 +1,9 @@
 import os
 from urllib.parse import urlparse
-from peewee import * #pylint: disable=unused-wildcard-import
+from peewee import *  # pylint: disable=unused-wildcard-import
 import math
 import datetime
+
 # from dotenv import load_dotenv
 
 # load_dotenv()
@@ -20,16 +21,14 @@ else:
     # db_name = os.environ["DB_NAME"]
     # db_user = os.environ["DB_USER"]
     # db_pword = os.environ["DB_PASSWORD"]
-    # db = PostgresqlDatabase(
-    #     db_name,
-    #     user=db_user,
-    #     password=db_pword,
-    # )
-    db = SqliteDatabase('test1.db')
+    db = PostgresqlDatabase(db_name, user=db_user, password=db_pword)
+    # db = SqliteDatabase('test1.db')
 
-class Base (Model):
+
+class Base(Model):
     class Meta:
         database = db
+
 
 class Floor(Base):
     floorNumber = IntegerField(primary_key=True)
@@ -48,43 +47,49 @@ class Floor(Base):
         except IntegrityError as e:
             print("insert error! ", e)
             raise ValueError("Floor Already Exists")
-    
+
     @classmethod
     def findFloor(cls, floorNumber):
-        found =  cls.get_or_none(Floor.floorNumber == floorNumber)
-        if (found != None):
+        found = cls.get_or_none(Floor.floorNumber == floorNumber)
+        if found != None:
             return found
         else:
             return None
 
     @property
     def numOfSeniors(self):
-        studentList = (Student.select()
-                            .join(Room)
-                            .where(Student.allocation==Room.roomNumber)
-                            .where(Room.floor==self.floorNumber)
-                            .where(Student.year>1))
-        
+        studentList = (
+            Student.select()
+            .join(Room)
+            .where(Student.allocation == Room.roomNumber)
+            .where(Room.floor == self.floorNumber)
+            .where(Student.year > 1)
+        )
+
         return studentList.count()
 
     @property
     def numOfFreshers(self):
-        studentList = (Student.select()
-                            .join(Room)
-                            .where(Student.allocation==Room.roomNumber)
-                            .where(Room.floor==self.floorNumber)
-                            .where(Student.year==1))
-        
+        studentList = (
+            Student.select()
+            .join(Room)
+            .where(Student.allocation == Room.roomNumber)
+            .where(Room.floor == self.floorNumber)
+            .where(Student.year == 1)
+        )
+
         return studentList.count()
 
     def numOfGender(self, isSenior=False):
         maleCount = 0
         femaleCount = 0
 
-        studentList = (Student.select()
-                            .join(Room)
-                            .where(Student.allocation==Room.roomNumber)
-                            .where(Room.floor==self.floorNumber))
+        studentList = (
+            Student.select()
+            .join(Room)
+            .where(Student.allocation == Room.roomNumber)
+            .where(Room.floor == self.floorNumber)
+        )
 
         for student in studentList:
             if isSenior and student.year == 1:
@@ -94,8 +99,8 @@ class Floor(Base):
                 maleCount += 1
             if student.gender == "f":
                 femaleCount += 1
-        
-        return {"m":maleCount, "f":femaleCount}
+
+        return {"m": maleCount, "f": femaleCount}
 
 
 class Room(Base):
@@ -111,30 +116,30 @@ class Room(Base):
     @classmethod
     def createRoom(cls, roomNum, bathroom, front, balc, rf, SubDivisionNumber):
         try:
-            floorNum = math.floor(roomNum/100)
+            floorNum = math.floor(roomNum / 100)
             newRoom = cls.create(
-                roomNumber = roomNum,
-                bathroom = bathroom,
-                front = front,
-                balc = balc,
-                rf = rf,
-                SubDivisionNumber = SubDivisionNumber,
-                assigned = False,
-                floor = floorNum
+                roomNumber=roomNum,
+                bathroom=bathroom,
+                front=front,
+                balc=balc,
+                rf=rf,
+                SubDivisionNumber=SubDivisionNumber,
+                assigned=False,
+                floor=floorNum,
             )
 
             return newRoom
         except IntegrityError:
             raise ValueError("Room Already Exists")
-    
+
     @classmethod
     def findRoom(cls, roomNumber):
-        found =  cls.get_or_none(Room.roomNumber == roomNumber)
-        if (found != None):
+        found = cls.get_or_none(Room.roomNumber == roomNumber)
+        if found != None:
             return found
         else:
             return False
-    
+
     # newOccupant as zid
     def assignRoom(self, newOccupant):
         self.assigned = True
@@ -143,7 +148,7 @@ class Room(Base):
         student.allocation = self.roomNumber
         self.save()
         student.save()
-    
+
     # oldOccupant as zid
     def clearAllocation(self):
         self.assigned = False
@@ -152,6 +157,7 @@ class Room(Base):
         student.allocation = None
         self.save()
         student.save()
+
 
 class Student(Base):
     zID = CharField(primary_key=True)
@@ -162,35 +168,36 @@ class Student(Base):
     assigned = BooleanField(null=True)
     allocation = ForeignKeyField(Room, backref="occupant", null=True)
     password = CharField()
-    startTime = DateTimeField(default=datetime.datetime.strptime("2050","%Y"))
+    startTime = DateTimeField(default=datetime.datetime.strptime("2050", "%Y"))
     otherPreferences = TextField(null=True)
 
     @classmethod
     def createStudent(cls, zid, name, year, gender, roomPoints, password, startTime):
         try:
             newStudent = cls.create(
-                zID = zid,
-                name = name,
-                year = year,
-                gender = gender,
-                roomPoints = roomPoints,
-                assigned = None,
-                allocation = None,
-                password = password,
-                startTime = startTime
+                zID=zid,
+                name=name,
+                year=year,
+                gender=gender,
+                roomPoints=roomPoints,
+                assigned=None,
+                allocation=None,
+                password=password,
+                startTime=startTime,
             )
 
             return newStudent
         except IntegrityError:
             raise ValueError("Student Already Exists")
-    
+
     @classmethod
     def findStudent(cls, zid):
-        found =  cls.get_or_none(Student.zID == zid)
-        if (found != None):
+        found = cls.get_or_none(Student.zID == zid)
+        if found != None:
             return found
         else:
             return False
+
 
 def db_reset():
     db.connect()
